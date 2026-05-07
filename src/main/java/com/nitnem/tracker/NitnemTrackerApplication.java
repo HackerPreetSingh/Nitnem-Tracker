cf0734e51b51a94e7029aabd078bc433ab28c0f4
Hempreet Singh <hempreetsingh001@gmail.com>
2026-05-07 13:51:42 +0530

    Initial Commit

---
+++ src/main/java/com/nitnem/tracker/NitnemTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.nitnem.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NitnemTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NitnemTrackerApplication.class, args);
+    }
+
+}
